c767f72e3ca2befdf60f826f9fd0e525d658e209
unknown <nazmulnobel000@gmail.com>
2022-10-12 17:40:06 +0600

    question header add

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -6,9 +6,11 @@ const Question = () => {
     const AllQuestion = useLoaderData()
     const Questions = AllQuestion.data;
     const{questions} = Questions
+    console.log(AllQuestion.data.name);
 
     return (
         <div className='md:ml-52 md:mr-52 mx-10'>
+            <h1 className="text-4xl mt-10 mb-10 text-lime-500">{AllQuestion.data.name} Related Question is here</h1>
             {
                 questions.map(ALLMCQ => <AllMCQ ALLMCQ={ALLMCQ}></AllMCQ>)
             }
